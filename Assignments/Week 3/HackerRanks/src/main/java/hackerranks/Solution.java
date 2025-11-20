99b3eed0385ae254379d40e942439c28555b91c9
ktc1176 <ktc1176@gmail.com>
2025-11-20 11:11:15 -0600

    hacker ranks

---
+++ Assignments/Week 3/HackerRanks/src/main/java/hackerranks/Solution.java
@@ -0,0 +1,31 @@
+package hackerranks;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+        int y = 0;
+
+        try{
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }catch (InputMismatchException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            System.out.println(x/y);
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
