559fa031ee5fd5a8d1b254a47f5e7865de15c809
ktc1176 <ktc1176@gmail.com>
2025-12-15 14:23:44 -0600

    rest assured demo

---
+++ Assignments/Week 6/RestAssuredDemo/src/test/java/com/revature/ra/DemoRestAssured01.java
@@ -0,0 +1,64 @@
+package com.revature.ra;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class DemoRestAssured01 {
+
+    @BeforeAll
+    static void setUp(){
+        RestAssured.baseURI="https://jsonplaceholder.typicode.com";
+    }
+
+    @AfterAll
+    static void tearDown(){
+        RestAssured.reset();
+    }
+
+    @Test
+    public void firstRequest(){
+
+        given()
+                .log().all()
+        .when()
+                .get("/posts/1")
+        .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testWithMoreDetails(){
+        given()
+                .log().parameters()
+                .queryParam("userId", 1)
+        .when()
+                .get("/posts") // Posts?userId=1
+        .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()",greaterThan(0));
+    }
+
+    @Test
+    public void testUsers1(){
+        given()
+                .log().parameters()
+        .when()
+                .get("/users/1")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", equalTo("Leanne Graham"))
+                //.body("email",hasToString("@"))
+                .body("address.city", equalTo("Gwenborough"))
+                .body("address.geo.lat", equalTo("-37.3159"));
+    }
+}
